12d99a7c1e64b71b35c878398db039dc130f0815
Paul Phyo <paulphyo25@gmail.com>
2025-06-28 15:57:42 +0800

    chore: enable CORS support for react +  add basic health check

--- 2025_PhyoNyiNyiPaing/src/main/java/com/paulphyo/CoinChangeApplication.java
+++ 2025_PhyoNyiNyiPaing/src/main/java/com/paulphyo/CoinChangeApplication.java
@@ -1,15 +1,22 @@
 package com.paulphyo;
 
+import com.paulphyo.health.BasicHealthCheck;
 import com.paulphyo.resources.CoinResource;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.FilterRegistration;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
+import java.util.EnumSet;
 
 public class CoinChangeApplication extends Application<CoinChangeConfiguration> {
 
     public static void main(final String[] args) throws Exception {
         new CoinChangeApplication().run(args);
     }
+
 
     @Override
     public String getName() {
@@ -24,5 +31,22 @@ public class CoinChangeApplication extends Application<CoinChangeConfiguration> 
     @Override
     public void run(CoinChangeConfiguration configuration, Environment environment) {
         environment.jersey().register(new CoinResource());
+        environment.healthChecks().register("basic", new BasicHealthCheck());
+        configureCors(environment);
+    }
+
+    private void configureCors(Environment environment) {
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin,Authorization");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        cors.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
     }
 }
---
+++ 2025_PhyoNyiNyiPaing/src/main/java/com/paulphyo/health/BasicHealthCheck.java
@@ -0,0 +1,10 @@
+package com.paulphyo.health;
+
+import com.codahale.metrics.health.HealthCheck;
+
+public class BasicHealthCheck extends HealthCheck {
+    @Override
+    protected Result check() throws Exception {
+        return Result.healthy();
+    }
+}
